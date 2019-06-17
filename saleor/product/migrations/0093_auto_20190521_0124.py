660095fb48c98018994918095dbf5fc664f4d345
Angelo Dini <angelo.dini@divio.ch>
2019-06-17 14:17:39 +0200

    split migrations

--- saleor/product/migrations/0093_auto_20190521_0124.py
+++ saleor/product/migrations/0093_auto_20190521_0124.py
@@ -31,15 +31,4 @@ class Migration(migrations.Migration):
                 to="product.Collection",
             ),
         ),
-        migrations.AlterModelTable(name="collectionproduct", table=None),
-        migrations.AlterField(
-            model_name="attributevalue",
-            name="sort_order",
-            field=models.PositiveIntegerField(db_index=True, editable=False, null=True),
-        ),
-        migrations.AlterField(
-            model_name="productimage",
-            name="sort_order",
-            field=models.PositiveIntegerField(db_index=True, editable=False, null=True),
-        ),
     ]
