71a8e7113598fc4b35e66640f5286b8e2bc15386
Angelo Dini <angelo.dini@divio.ch>
2019-09-03 11:08:26 +0200

    bumped version to 2.8.0.7

--- saleor/celeryconf.py
+++ saleor/celeryconf.py
@@ -1,6 +1,8 @@
 import os
 
 from celery import Celery
+from django import settings
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saleor.settings")
 
@@ -8,5 +10,8 @@ app = Celery("saleor")
 
 CELERY_TIMEZONE = "UTC"
 
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
+# Divio Cloud custom changes, as we use Celery <4
+# app.config_from_object("django.conf:settings", namespace="CELERY")
+# app.autodiscover_tasks()
+app.config_from_object("django.conf:settings")
+app.autodiscover_tasks(settings.INSTALLED_APPS)
