aee43964c5fd4cc7dd517d6c64f3207279957247
Angelo Dini <angelo.dini@divio.ch>
2019-10-28 09:27:48 +0100

    updated root files

--- update_saleor.sh
+++ update_saleor.sh
@@ -20,22 +20,21 @@ rm -rf templates/
 
 echo "----- Copy files -----"
 \cp -Rf \
+    tmp_saleor/.dockerignore \
     tmp_saleor/.editorconfig \
     tmp_saleor/.eslintrc.json \
-    tmp_saleor/.gqlconfig \
+    tmp_saleor/.flowconfig \
+    tmp_saleor/.isort.cfg \
     tmp_saleor/.pylintrc \
-    tmp_saleor/apollo.config.js \
     tmp_saleor/app.json \
+    tmp_saleor/common.env \
     tmp_saleor/package-lock.json \
     tmp_saleor/package.json \
-    tmp_saleor/Pipfile \
-    tmp_saleor/Pipfile.lock \
+    tmp_saleor/poetry.lock \
     tmp_saleor/requirements_dev.txt \
     tmp_saleor/requirements.txt \
-    tmp_saleor/tsconfig.json \
-    tmp_saleor/tslint.json \
+    tmp_saleor/setup.cfg \
     tmp_saleor/webpack.config.js \
-    tmp_saleor/webpack.d.ts \
     .
 
 echo "----- Cleaning up -----"
